1db6fffe99d7742d66606ff0ce9b1317adad039f
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-02 14:54:33 +0200

    Reader: Testing all single numbers
    Compilation works when testing 0-9 as single numbers
    
    Issue #1
    
    Signed-off-by: Lorenzo Milesi <lorenzo.milesi@live.fr>

--- src/test/Reader.test.ts
+++ src/test/Reader.test.ts
@@ -9,6 +9,15 @@ describe('Reader Test', () => {
 
     it('Should parse single numbers', () => {
         expect(reader.read(numbers.ZERO)).to.equal(0);
+        expect(reader.read(numbers.ONE)).to.equal(1);
+        expect(reader.read(numbers.TWO)).to.equal(2);
+        expect(reader.read(numbers.THREE)).to.equal(3);
+        expect(reader.read(numbers.FOUR)).to.equal(4);
+        expect(reader.read(numbers.FIVE)).to.equal(5);
+        expect(reader.read(numbers.SIX)).to.equal(6);
+        expect(reader.read(numbers.SEVEN)).to.equal(7);
+        expect(reader.read(numbers.EIGHT)).to.equal(8);
+        expect(reader.read(numbers.NINE)).to.equal(9);
     });
 
 });
